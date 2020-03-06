62be628b85e361b018131c4415885960cccf5e4a
B. Bueno <bbueno25@hotmail.com>
2020-03-06 11:20:45 -0700

    Intro and Getting Stock Price Data - Python Programming for Finance p.1

--- finance_programming/finance_programming.py
+++ finance_programming/finance_programming.py
@@ -1,1 +1,22 @@
+"""
+DOCSTRING
+"""
+import datetime
+import matplotlib.pyplot as pyplot
+import matplotlib.style as style
+import pandas
+import pandas_datareader.data as web
 
+style.use('ggplot')
+
+def getting_data():
+    """
+    DOCSTRING
+    """
+    start = datetime.datetime(2000, 1, 1)
+    end = datetime.datetime(2016, 12, 31)
+    dataframe_a = web.DataReader('GOOGL', 'yahoo', start, end)
+    print(dataframe_a.head())
+
+if __name__ == '__main__':
+    getting_data()
