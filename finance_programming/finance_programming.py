898c73ffcca79e4fe5915e528bc61497df6bc36c
B. Bueno <bbueno25@hotmail.com>
2020-03-07 15:08:40 -0700

    More stock manipulations - Python Programming for Finance p.4

--- finance_programming/finance_programming.py
+++ finance_programming/finance_programming.py
@@ -2,8 +2,10 @@
 DOCSTRING
 """
 import datetime
+import matplotlib.dates as mdates
 import matplotlib.pyplot as pyplot
 import matplotlib.style as style
+import mplfinance.original_flavor as mplfinance
 import pandas
 import pandas_datareader.data as web
 
@@ -31,12 +33,15 @@ def manipulate_data():
     DOCSTRING
     """
     dataframe_a = pandas.read_csv('google.csv', parse_dates=True, index_col=0)
-    dataframe_a['100ma'] = dataframe_a['Adj Close'].rolling(window=100).mean()
+    dataframe_b = dataframe_a['Adj Close'].resample('10D').ohlc()
+    dataframe_b.reset_index(inplace=True)
+    dataframe_b['Date'] = dataframe_b['Date'].map(mdates.date2num)
+    dataframe_c = dataframe_a['Volume'].resample('10D').sum()
     axis_1 = pyplot.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+    axis_1.xaxis_date()
+    mplfinance.candlestick_ohlc(axis_1, dataframe_b.values, width=2, colorup='g')
     axis_2 = pyplot.subplot2grid((6, 1), (5, 0), rowspan=5, colspan=1, sharex=axis_1)
-    axis_1.plot(dataframe_a.index, dataframe_a['Adj Close'])
-    axis_1.plot(dataframe_a.index, dataframe_a['100ma'])
-    axis_2.bar(dataframe_a.index, dataframe_a['Volume'])
+    axis_2.fill_between(dataframe_c.index.map(mdates.date2num), dataframe_c.values, 0)
     pyplot.show()
 
 if __name__ == '__main__':
