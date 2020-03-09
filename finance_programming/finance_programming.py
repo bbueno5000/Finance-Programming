2f9488335297311e7c0a8d474a6db08c1f550977
B. Bueno <bbueno25@hotmail.com>
2020-03-09 10:19:32 -0600

    Getting all company pricing data in the S&P 500 - Python Programming for Finance p.6

--- finance_programming/finance_programming.py
+++ finance_programming/finance_programming.py
@@ -7,6 +7,7 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as pyplot
 import matplotlib.style as style
 import mplfinance.original_flavor as mplfinance
+import os
 import pandas
 import pandas_datareader.data as web
 import pickle
@@ -22,6 +23,27 @@ def get_data():
     end = datetime.datetime(2016, 12, 31)
     dataframe_a = web.DataReader('GOOGL', 'yahoo', start, end)
     dataframe_a.to_csv('google.csv')
+
+def get_data_from_yahoo(reload_sp500=False):
+    """
+    DOCSTRING
+    """
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500_tickers.pkl', 'rb') as file_a:
+            tickers = pickle.load(file_a)
+    if not os.path.exists('stock_dataframes'):
+        os.makedirs('stock_dataframes')
+    start = datetime.datetime(2000, 1, 1)
+    end = datetime.datetime(2016, 12, 31)
+    for ticker in tickers[:10]:
+        print(ticker)
+        if not os.path.exists('stock_dataframes/{}.csv'.format(ticker)):
+            dataframe_a = web.DataReader(ticker, 'yahoo', start, end)
+            dataframe_a.to_csv('stock_dataframes/{}.csv'.format(ticker))
+        else:
+            print('{} already exists'.format(ticker))
 
 def graph_data():
     """
@@ -57,9 +79,9 @@ def save_sp500_tickers():
     tickers = []
     for row in table.findAll('tr')[1:]:
         tickers.append(row.findAll('td')[0].text)
-    with open('sp500_tickers.pkl', 'wb') as file:
-        pickle.dump(tickers, file)
+    with open('sp500_tickers.pkl', 'wb') as file_a:
+        pickle.dump(tickers, file_a)
     return tickers
 
 if __name__ == '__main__':
-    save_sp500_tickers()
+    get_data_from_yahoo()
